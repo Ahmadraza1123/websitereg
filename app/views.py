6e3fb18abc9e29a2046db178e79e10b1bdb369e5
Ahmadraza1123 <ahmadraza19397@gmail.com>
2025-08-29 18:14:11 +0500

    2comit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from .models import Student
+from .serializers import StudentSerializer
 
-# Create your views here.
-jnnjndajjndajxndsanq saQJQ
+class studentAPIView(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
